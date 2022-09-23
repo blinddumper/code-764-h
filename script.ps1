35a6ebdfea6cec636c6bdd251ca538fa02897652
blinddumper <blinddumper@yahoo.com>
2022-09-23 17:01:10 +0100

    Improved error handling logic

--- script.ps1
+++ script.ps1
@@ -8,3 +8,4 @@ Temporary commit content for 2022-08-08T17:01:10
 Temporary commit content for 2022-08-14T17:01:10
 Temporary commit content for 2022-09-02T17:01:10
 Temporary commit content for 2022-09-07T17:01:10
+Temporary commit content for 2022-09-23T17:01:10
