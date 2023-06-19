d3f2a2510382fec2dd04b2ea37fea0744eca1c42
blinddumper <blinddumper@yahoo.com>
2023-06-19 17:01:10 +0100

    Fixed typos in documentation

--- script.ps1
+++ script.ps1
@@ -36,3 +36,4 @@ Temporary commit content for 2023-05-03T17:01:10
 Temporary commit content for 2023-05-06T17:01:10
 Temporary commit content for 2023-05-20T17:01:10
 Temporary commit content for 2023-06-11T17:01:10
+Temporary commit content for 2023-06-19T17:01:10
