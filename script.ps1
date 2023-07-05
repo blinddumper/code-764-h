bca3e153b0fb88c13333c5a0f797742a60734fde
blinddumper <blinddumper@yahoo.com>
2023-07-05 17:01:10 +0100

    Added validation for user input

--- script.ps1
+++ script.ps1
@@ -37,3 +37,4 @@ Temporary commit content for 2023-05-06T17:01:10
 Temporary commit content for 2023-05-20T17:01:10
 Temporary commit content for 2023-06-11T17:01:10
 Temporary commit content for 2023-06-19T17:01:10
+Temporary commit content for 2023-07-05T17:01:10
